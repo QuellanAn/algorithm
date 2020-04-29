01839f015f21ebbc4d962b11909ce04b7de2990f
QuellanAn <1186154608@qq.com>
2020-04-29 10:54:03 +0800

    leetcode

---
+++ src/main/java/leetcode/day17/MountainArray.java
@@ -0,0 +1,7 @@
+package leetcode.day17;
+
+public interface MountainArray {
+    public int get(int index);
+    public int length();
+
+}
---
+++ src/main/java/leetcode/day17/MountainArrayImpl.java
@@ -0,0 +1,28 @@
+package leetcode.day17;
+
+import java.util.List;
+
+/**
+ * @ClassName MountainArrayImpl
+ * @Description DOTO
+ * @Author zhulinfeng
+ * @Date 2020/4/29 10:06
+ * @Version 1.0
+ */
+public class MountainArrayImpl implements MountainArray {
+
+    public List<Integer> list;
+
+    public MountainArrayImpl(List<Integer> list){
+        this.list=list;
+    }
+    @Override
+    public int get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public int length() {
+        return list.size();
+    }
+}
